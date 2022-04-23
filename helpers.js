ba942db99f98d6b81603fc01f34439d10cec8084
Rayen <104236866+Rayen60@users.noreply.github.com>
2022-04-23 01:59:04 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ **:  `🅡🅐🅨🅔🅝``" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *🅡🅐🅨🅔🅝*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ **:  `🅡🅐🅨🅔🅝``" + msg + "```"
 }
 
 
